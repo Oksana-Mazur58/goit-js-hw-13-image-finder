4d9c8ac33e13604ce4f6c8e05066d91b3ff3914c
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-04-29 16:31:37 +0300

    додала скрол

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import './styles.css';
 import imageListTemplates from './templates/imageList.hbs'
 import NewsApiServise from './js/api-service'
+import LoadMoreBtn from './js/load-more-btn'
 
 
 const refs = {
     searchForm: document.querySelector('.seaech_form'),
     imageList: document.querySelector('.images-list'),
     readMoreBtn: document.querySelector('[data-action="load-more"]'),
-    resetBtn: document.querySelector('[data-action="reset"]')
+    // resetBtn: document.querySelector('[data-action="reset"]')
 }
+// const loadMoreBtn = new LoadMoreBtn({
+//     selector: '.load-more-btn',
+//     hidden: true,
+// })
+
+
+
+
 const newsApiServise = new NewsApiServise();
 
 
@@ -24,16 +33,26 @@ function onSearch(e) {
     newsApiServise.query = e.currentTarget.elements.query.value
     if (newsApiServise.query === '') {
         clearImageContainer();
-        alert('Введіть дані')
-    } else {clearImageContainer();
+        alert('Введіть дані');
+        // loadMoreBtn.hide();
+    } else {
+        clearImageContainer();
+        // loadMoreBtn.show();
         newsApiServise.resetPage()
-        onReadMore()
+        newsApiServise.fetchImage().then(({ hits }) => {
+            imagesMarkup(hits);
+        })
+        // onReadMore()
     }
 }
 
 function onReadMore() {
+    // loadMoreBtn.disable();
     newsApiServise.fetchImage().then(({ hits }) => {
-        imagesMarkup(hits)
+        imagesMarkup(hits);
+
+        windowsScrolling()
+    
     })
 }
 
@@ -43,5 +62,15 @@ function imagesMarkup (result){
 }
 function clearImageContainer() {
     refs.imageList.innerHTML = '';
+}
+
+function windowsScrolling() {
+const totalScrollHeight = document.body.clientHeight;
+
+ window.scrollTo({
+  top: totalScrollHeight,
+  left: 0,
+  behavior: 'smooth',
+});
 }
 
